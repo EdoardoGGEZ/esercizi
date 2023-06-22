e7a5d0a51c63d567d621bf22df864005bd98f5ea
Edoardo Gini <edoardo.gini.stud@tulliobuzzi.edu.it>
2023-06-22 11:59:11 +0200

    ho tradotto tutte le variabili in inglese

--- cpp/es1.1/mediaEdoardo.cpp
+++ cpp/es1.1/mediaEdoardo.cpp
@@ -8,22 +8,27 @@ int main()
     int length;
     int sum = 0;
     int average;
+
+    // controllo input
     do
     {
         cout << "Quanti numeri vuoi summare e calcolarne la average? minimo 2\n";
         cin >> length;
-    } while (length < 2); // controllo input
+    } while (length < 2); 
 
+    // input numeri e somma
     for (int i = 0; i < length; i++)
-    { // input numeri e summa
+    { 
         cout << "Numero " << i + 1 << ": ";
         int app;
         cin >> app;
         sum += app;
     }
 
-    average = sum / length; // calcolo average
+    // calcolo average
+    average = sum / length; 
 
+    //stampa
     cout << "summa: " << sum << endl
          << "average: " << average << endl;
     return 0;
