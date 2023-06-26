a641386fc0916990cef19824dd0420a793544da3
Edoardo Gini <edoardo.gini.stud@tulliobuzzi.edu.it>
2023-06-26 16:48:52 +0200

    correzione di ottaviano

--- cpp/es1.3/modaEdoardo.cpp
+++ cpp/es1.3/modaEdoardo.cpp
@@ -2,6 +2,7 @@
 #include<fstream>
 #include<vector>
 #include<string>
+#include <map>
 //MAX parole più ripetute
 #define MAX 10
 
@@ -19,7 +20,7 @@ void bubleSort(vector<Word> &vector){
     bool esci;
     do{
         esci=true;
-        for(int i=0;i<vector.size();i++){
+        for(int i=0;i<vector.size()-1;i++){
             if(vector[i].nTimes<vector[i+1].nTimes){
                 swap(vector[i],vector[i+1]);
                 esci=false;
@@ -35,36 +36,34 @@ int main(){
     //variabili
     vector<Word> words;
 
+    std::map<std::string, int> words_map;
+
     //input parole
     if(input.is_open()){
-    do{
-        Word add={"",1};
-        char letter;
-        add.word.clear();
-        letter=input.get();
-        while(letter!=' '&&letter!='\n'&&letter!='.'&&letter!=','&&letter!=';'&&letter!=':'&&letter!='\''&&letter!='!'&&letter!='\"'){
-            add.word+=letter;
+        while (!input.eof()) {
+            Word add={"",1};
+            int letter;
+            add.word.clear();
             letter=input.get();
+            if (letter == std::char_traits<char>::eof())
+                break;
+            while(letter!=' '&&letter!='\n'&&letter!='.'&&letter!=','&&letter!=';'&&letter!=':'&&letter!='\''&&letter!='!'&&letter!='\"'){
+                add.word+=letter;
+                letter=input.get();
+            }
+            if(add.word.size()>3){
+                words_map[add.word] ++;
+            }
         }
-        if(add.word.size()>3){
-            words.push_back(add);
-        }
-    }
-    while(input.eof());
-    input.close();
-    } else{
+        input.close();
+    } else {
         std::cout<<"file mancante!";
     }
     std::cout<<"[input]\n";
     //ciclo conteggio parole
-    for(int i=0;i<words.size();i++){
-        string app=words[i].word;
-        for(int j=i+1;i<words.size();j++){
-            if(words[j].word==app){
-                words[i].nTimes++;
-                words.erase(words.begin()+j);
-            }
-        }
+
+    for (const auto &pair : words_map) {
+        words.push_back({pair.first, pair.second});
     }
     std::cout<<"[conteggio]\n";
     //riordina i numeri
@@ -72,7 +71,7 @@ int main(){
     std::cout<<"[sort]\n";
     //stampa le MAX parole più ripetute
     for(int i=0;i<MAX;i++){
-        std::cout<<i+1<<" parola piu' ripetuta: "<<words[i].word;
+        std::cout<<i+1<<" parola piu' ripetuta: "<<words[i].word << endl;
     }
     return 0;
 }
